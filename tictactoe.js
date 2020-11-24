0472d5a73414e80c6705f970a6ed56b0877f13c7
vincentyang01 <vincentyangcsi@gmail.com>
2020-11-24 17:41:57 -0500

    first commit

---
+++ tictactoe.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+const board = document.querySelector('.board');
+const square = document.querySelectorAll('.square');
+const button = document.querySelector('.button');
+
+function turn() {
+    square.addEventListener('click', function(){
+        
+    })
+}
