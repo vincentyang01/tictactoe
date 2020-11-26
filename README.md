385efef42b7b3adf6fbc0c26630589811b45c727
vincentyang01 <vincentyangcsi@gmail.com>
2020-11-26 01:41:54 -0500

    Font alignment fixed. Font style is not Avenir as in design specs. README is updated.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# fourLargest.js
+I used repl.it in order to verify that it works.
+
 # tictactoe
+To run Tic-Tac-Toe, copy the path of tictactoe.html into your browser.
--- fourLargest.js
+++ fourLargest.js
@@ -1,8 +1,8 @@
-function sort(arr){
+function fourLargest(arr){
     arr.sort(function(a, b){
         return a - b
     })
     return [arr[arr.length-1], arr[arr.length-2], arr[arr.length-3], arr[arr.length-4]]
 }
 
-sort([4,20,6,28,50,22,13,31,5,1,25])
+fourLargest([4,20,6,28,50,22,13,31,5,1,25])
